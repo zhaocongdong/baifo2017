004a6043f625cb577980dd88cd04da6e7d71220d
cc2059 <zhaocongdong@hotmail.com>
2017-04-29 13:44:46 +0800

    create baifo2017

---
+++ module/alogin/view/index.html.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The html template file of index method of index module of zentaoPHP.
+ *
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ *
+ *  May you do good and not evil.
+ *  May you find forgiveness for yourself and forgive others.
+ *  May you share freely, never taking more than you give.
+ */
+?>
+<?php include '../../common/view/header.html.php';?>
+<div class='container'>
+    登录
+</div>
+<?php include '../../common/view/footer.html.php';?>
