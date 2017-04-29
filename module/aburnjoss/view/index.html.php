98ac7ab19194e4fdce7c832758fa1972eb722af2
张振凯 <zhangzhenkai@zhangzhenkaideMacBook-Pro.local>
2017-04-29 18:33:25 +0800

    dev001

--- module/aburnjoss/view/index.html.php
+++ module/aburnjoss/view/index.html.php
@@ -13,12 +13,37 @@
 <?php include '../../common/view/header.html.php';?>
 <div class='container'>
     <div class="content">
-        <div class="shaoxiang">
-            <div class="fo-lists-coantainer">
+        <div class="aburnjoss-container">
+            <div class="fo-lists-container">
                 <ul class="fo-lists">
-                    <li></li>
+                    <li class="fo1"></li>
+                    <li class="fo2"></li>
+                    <li class="fo3"></li>
+                    <li class="fo4"></li>
+                    <li class="fo5"></li>
+                    <li class="fo6"></li>
                 </ul>
+                <div class="fo-handle">
+                    <input type="button" class="arrow-left" />
+                    <input type="button" class="arrow-right" />
+                </div>
             </div>
+            <div class="tribute-lists-container">
+                <div class="tribute-lists-bg">
+                    <ul>
+
+                    </ul>
+                </div>
+                <div class="tribute-handle">
+                    <input type="button" class="arrow-left" />
+                    <input type="button" class="arrow-right" />
+                </div>
+                <div class="tribute-confirm-submit">
+                    <p></p>
+                    <input type="button" value="确定上香" />
+                </div>
+            </div>
+            <div class="merit-box"></div>
         </div>
     </div>
 </div>
