bde7cb7756788b237ea3a775a91babe290e9d623
cc2059 <zhaocongdong@hotmail.com>
2017-05-03 21:30:31 +0800

    opAnimal

--- module/afreeanimal/control.php
+++ module/afreeanimal/control.php
@@ -34,4 +34,24 @@ class afreeanimal extends control
         $this->view->pageJS = $this->app->getWebRoot() . "assets/js/afreeanimal.js";
         $this->display();
     }
+
+    public function opAnimal() {
+        if (!empty($_POST)) {
+            var_dump($_POST);
+            $opinfo = null;
+            $opinfo->uid        = $_POST["uid"];
+            $opinfo->aname      = $_POST["aname"];
+            $opinfo->num        = $_POST["num"];
+            $opinfo->price      = $_POST["price"];
+            $opinfo->total      = $_POST["total"];
+            $opinfo->img_list   = $_POST["img_list"];
+            $opinfo->img_stand  = $_POST["img_stand"];
+            $opinfo->img_move   = $_POST["img_move"];
+            $opinfo->item_name  = $_POST["item_name"];
+            $opinfo->is_buy     = $_POST["is_buy"];
+            $opinfo->time       = date(DATE_FORMAT);
+            var_dump($opinfo);return;
+            $this->dao->insert('bf_user_animal_op')->data($opinfo)->exec();
+        }
+    }
 }
