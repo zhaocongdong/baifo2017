a4c97e1e712fcfa0ed2dea02104b3fe09264880b
张振凯 <zhangzhenkai@zhangzhenkaideMacBook-Pro.local>
2017-05-11 23:45:38 +0800

    dev001

--- assets/js/afreeanimal.js
+++ assets/js/afreeanimal.js
@@ -1,8 +1,8 @@
 AFreeAnimal.start();
 dataController.getUserInfo(function(res){
     if(res.name){
-        $(".nickname").html("银两："+res.name+"两");
-        $(".title").html("银两："+res.title+"两");
+        $(".nickname").html("昵称："+res.name+"两");
+        $(".title").html("称呼："+res.title+"两");
         $(".balance").html("银两："+res.gold_num+"两");
     }
 });
